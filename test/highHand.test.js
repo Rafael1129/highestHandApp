532f681c52d60c203fcb029789daa99bf3f15521
Rafael Pereira <rafael_ac_pereira@outlook.com>
2018-09-24 20:36:16 -0400

    modified some tests

--- test/highHand.test.js
+++ test/highHand.test.js
@@ -16,22 +16,22 @@ describe('highestHand', function() {
     });
 
     it('should return One pair hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ 'JS', 'AD', '5C', 'AS', '3D' ];
       assert.equal(highHand.highestHand(items),'One pair');
     });
 
     it('should return Two pair hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ 'JS', 'AD', 'AC', '4S', '4D' ];
       assert.equal(highHand.highestHand(items),'Two pair');
     });
 
     it('should return Three of a kind hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ 'JS', 'JD', 'JC', '4S', '3D' ];
       assert.equal(highHand.highestHand(items),'Three of a kind');
     });
 
     it('should return Straight hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ '2S', '3D', '4C', '5S', '6D' ];
       assert.equal(highHand.highestHand(items),'Straight');
     });
 
@@ -41,17 +41,17 @@ describe('highestHand', function() {
     });
 
     it('should return Full house hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ 'JS', 'JD', 'JC', '4S', '4D' ];
       assert.equal(highHand.highestHand(items),'Full house');
     });
 
     it('should return Four of a kind hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ 'JS', 'JD', 'JC', 'JS', '3D' ];
       assert.equal(highHand.highestHand(items),'Four of a kind');
     });
 
     it('should return Straight flush hand', function() {
-      items = [ 'JS', 'AD', '5C', '4S', '3D' ];
+      items = [ '2S', '3S', '4S', '5S', '6S' ];
       assert.equal(highHand.highestHand(items),'Straight flush');
     });
 
